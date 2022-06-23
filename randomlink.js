07d0e136380e0c73017ce46f998105f64ea0f522
music4me <taufan1102pro@gmail.com>
2022-06-23 09:57:41 +0700

    Added

---
+++ randomlink.js
@@ -0,0 +1,26 @@
+var Randomlinks=new Array()
+  
+Randomlinks[0]="https://www.youtube.com/watch?v=yW2udM76yYk"
+Randomlinks[1]="https://www.youtube.com/watch?v=B2FxFzAEgkM"
+Randomlinks[2]="https://www.youtube.com/watch?v=sPd3-PdsOZM"
+Randomlinks[3]="https://www.youtube.com/watch?v=GIgLqN_rAXU"
+Randomlinks[4]="https://www.youtube.com/watch?v=_au37MZaQQI"
+Randomlinks[5]="https://www.youtube.com/watch?v=kgE2jDVPVXE&ab_channel=Th%E1%BA%A7nTho%E1%BA%A1iCh%C3%A9mGi%C3%B3"
+Randomlinks[6]="https://www.youtube.com/watch?v=4Qx9onV1xY8ab_channel=%E1%BB%9E%C4%90%C3%A2yC%C3%B3Tu%E1%BB%95iTh%C6%A1N%C3%A8"
+Randomlinks[7]="https://youtu.be/g0yoTBS-kk0"
+Randomlinks[8]="https://youtu.be/YB4yL_0R4hA"
+Randomlinks[9]="https://youtu.be/1CUFVo0V3_Q"
+Randomlinks[10]="https://youtu.be/w1FgmqTU92I"
+Randomlinks[11]="https://youtu.be/X2DUpDxFJyg"
+Randomlinks[12]="https://youtu.be/gAqaU6dBA_E"
+Randomlinks[13]="https://youtu.be/3xlS4AKEF9Y"
+Randomlinks[14]="https://youtu.be/v18G3XNLwJc"
+Randomlinks[15]="https://youtu.be/h-YirW_7cDY"
+Randomlinks[16]="https://youtu.be/6TtZkS5SaBk"
+Randomlinks[17]="https://youtu.be/ykWNk_U-Pmg"
+Randomlinks[18]="https://youtu.be/6fK4RLs9vK8"
+Randomlinks[19]="https://youtu.be/JPM2rBed8t8"
+
+function randomlink(){
+window.location=Randomlinks[Math.floor(Math.random()*Randomlinks.length)]
+}
